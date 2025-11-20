f84b9b815fa8cb61b3c3dcc67035160aca7b945b
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2025-11-20 18:10:17 -0300

    novo link api

--- my-app/src/utils/api.tsx
+++ my-app/src/utils/api.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://frotinix.eastus2.cloudapp.azure.com/',
+  // baseURL: 'http://frotinix.eastus2.cloudapp.azure.com/',
+  baseURL: 'https://frotinix.duckdns.org/',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
